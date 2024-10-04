73e9529e297c1bedc9ff18c7a0fd7778b9b12fe9
Leonardo Hernandes T. de S. Filho <leonardo.filho@ntsec.corp>
2024-10-04 11:47:35 -0300

    feat: add user migration

---
+++ src/database/migrations/1728052943989-CreateUsers.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsers1728052943989 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "username",
+            type: "varchar",
+            length: "60",
+          },
+          {
+            name: "usernickname",
+            type: "varchar",
+            length: "60",
+            isUnique: true,
+          },
+          {
+            name: "email",
+            type: "varchar",
+            length: "254",
+            isUnique: true,
+          },
+          {
+            name: "birthday",
+            type: "date",
+          },
+          {
+            name: "password",
+            type: "varchar",
+            length: "60",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
